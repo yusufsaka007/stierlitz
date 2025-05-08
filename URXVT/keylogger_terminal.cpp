942eca068bef3a52dd7b37425a676697c63d8fe8
yusufsaka007 <yusufsaka5481@gmail.com>
2025-05-08 07:50:44 -0500

    Deleted unnecessary array

--- URXVT/keylogger_terminal.cpp
+++ URXVT/keylogger_terminal.cpp
@@ -11,8 +11,6 @@
 #include <termios.h>
 #include <xkbcommon/xkbcommon.h>
 
-const char* keycodes[] = {"[RESERVED]", "[ESC]", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "[MINUS]", "[EQUAL]", "[BACKSPACE]", "TAB", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[LEFTBRACE]", "[RIGHTBRACE]", "[ENTER]", "[LEFTCTRL]", "A", "S", "D", "F", "G", "H", "J", "K", "L", "[SEMICOLON]", "[APOSTROPHE]", "[GRAVE]", "[LEFTSHIFT]", "[BACKSLASH]", "Z", "X", "C", "V", "B", "N", "M", "[COMMA]", "[DOT]", "[SLASH]", "[RIGHTSHIFT]", "[KPASTERISK]", "[LEFTALT]", "[SPACE]", "[CAPSLOCK]", "[F1]", "[F2]", "[F3]", "[F4]", "[F5]", "[F6]", "[F7]", "[F8]", "[F9]", "[F10]", "[NUMLOCK]", "[SCROLLLOCK]"};
-
 #define GREEN   "\033[32m"
 #define RED     "\033[31m"
 #define MAGENTA "\033[35m"
